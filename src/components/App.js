3b1ef600fd2c7471fce9d0bf79a3386334ee1505
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-05-20 15:05:40 +0300

    React Simple Data Fetching Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    async function fetchDogImage() {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setDogImage(data.message);
+    }
+
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {!dogImage ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
